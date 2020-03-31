ccfab019b561d8b53a0d9251c1891441ddcbf846
Peter Brown <phbrown@acm.org>
2020-03-31 18:31:29 -0400

    Added starter files

---
+++ TestNothing.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+#
+#  testnothing.py: empty unit-testing class
+#  
+#  Copyright 2018 Peter Brown <peter.brown@converse.edu>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+
+
+import unittest
+# Import the module(s) you want to test here
+
+
+class TestNothing(unittest.TestCase):
+
+    def test_nothing(self) -> None:
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ do_nothing.py
@@ -0,0 +1,11 @@
+# Do nothing, successfully
+# Peter Brown, 2020-01-07
+
+from typing import List
+
+def main(args:List[str]) -> int:
+    return 0
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main(sys.argv))
